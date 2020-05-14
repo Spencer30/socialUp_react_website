a4c0b440a389626553468f44e00becf1e229f46b
Spencer Lyons <sjl8r8@gmail.com>
2020-05-14 15:18:22 -0500

    fixed avatar styling

--- client/src/components/AppJumbotron.jsx
+++ client/src/components/AppJumbotron.jsx
@@ -17,15 +17,15 @@ const AppJumbotron = () => {
           </div>
           <div style={{position:'absolute', top:'25%', width:'100%', display:'flex', justifyContent:'space-evenly', flexWrap:'wrap'}}>
           <AvatarGroup team={1} name="Basketball Crew"/>
-          <AvatarGroup team={2} name="Nature & Hiking"/>
-          <AvatarGroup team={3} name="Yoga & Dance"/>
-          <AvatarGroup team={4} name="Happy Hours"/>
-          <AvatarGroup team={5} name="Volunteer Team"/>
-          <AvatarGroup team={6} name="Basketball Crew"/>
-          <AvatarGroup team={5} name="Nature & Hiking"/>
-          <AvatarGroup team={4} name="Yoga & Dance"/>
-          <AvatarGroup team={3} name="Happy Hours"/>
-          <AvatarGroup team={2} name="Volunteer Team"/>
+          <AvatarGroup team={2} name="Chiefs Parties"/>
+          <AvatarGroup team={3} name="Coffee Chats"/>
+          <AvatarGroup team={6} name="Dinner Club"/>
+          <AvatarGroup team={5} name="Happy Hours"/>
+          <AvatarGroup team={4} name="Nature & Hiking"/>
+          <AvatarGroup team={3} name="Volunteer Team"/>
+          <AvatarGroup team={2} name="Weekend Meetups"/>
+          <AvatarGroup team={1} name="Yoga & Dance"/>
+          <AvatarGroup team={6} name="Young Professionals"/>
           </div>
         </div>
         </div>
