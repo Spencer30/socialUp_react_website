d315c4822d316b0e0dcde8f75c182b117e0e7284
Spencer Lyons <sjl8r8@gmail.com>
2020-06-29 09:11:46 -0500

    added in few packages and testing to make sure env file doesn't push to github.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser')
+const mysql = require('mysql');
+const dotenv = require('dotenv');
 let port = process.env.PORT;
-
+dotenv.config({path:'./.env'});
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+var db = mysql.createPool({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE
+});
+
+//Connect DB
+db.getConnection((err) => {
+  if(err) throw err;
+  console.log('MySql Connected');
+})
 
 app.use(express.static(path.join(__dirname, './client/build')));
 app.get('/', function(req, res) {
@@ -13,9 +28,10 @@ app.get('/', function(req, res) {
 });
 
 app.post('/contact', (req, res) => {
-  
+  console.log(req.body.data)
   res.status(200).send('Your Message Has Been Sent!')
 })
+
 
 
 if (port == null || port == "") {
